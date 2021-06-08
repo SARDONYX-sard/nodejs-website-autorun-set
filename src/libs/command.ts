9a8483d4562a6f75cc82bf2a27d818a50309a30a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-06-08 14:48:37 +0900

    [Modify] Modify execCommand functon

--- src/libs/command.ts
+++ src/libs/command.ts
@@ -12,25 +12,36 @@ export function echoError(error: Error): void {
 /**
  * Function to execute Windows commands.
  * @param command - Execute command
- * @param args - option: Command argument
+ * @param args - option: Command arg
  *
  * @example:pause script
  * // 1. In sample.ts...
- * execCommand("pause")
+ * execCommand("pause", --pause)
  *
  * // 2. In terminal...
- * npx ts-node src/selenium/sample/sample.ts pause
+ * npx ts-node src/selenium/sample/sample.ts --pause
  */
-export function execCommand(command: string, arg: string = command): void {
-  // const argument = arg ?? command;
-  if (RegExp(arg).test(process.argv[2])) {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`[ERROR] ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
+export function execCommand(command: string, arg: string | RegExp = command): void {
+  // if arg is string
+  if (typeof arg === "string" && arg === process.argv[2]) {
+    execute(command);
+    return;
   }
+
+  // if arg is RegExp
+  if (arg instanceof RegExp && arg.test(process.argv[2])) {
+    execute(command);
+    return;
+  }
+}
+
+function execute(command: string): void {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`[ERROR] ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
