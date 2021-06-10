043946cd79b5d62f295d87f00bfd830c302f7479
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-06-10 21:01:26 +0900

    [Clean] Refactoring sample.ts to main.ts

--- src/selenium/main.ts
+++ src/selenium/main.ts
@@ -1,12 +1,60 @@
-import { Manga } from "./custom/custom-selenium";
+import moment from "moment";
+import { By, WebDriver } from "selenium-webdriver";
 // common libs
-import { echoError } from "../libs";
+import { echoError, execCommand, writeFiles } from "../libs";
+// selenium libs
+import { build, getUrlContent } from "./libs";
 
-(async () => {
-  const options = {
-    args: ["--headless", "--disable-gpu", "--window-size=1024,768"],
+// --------loop Tab sample function----------
+// import { loopTab } from "../libs";
+
+// async function loopUrl() {
+//   await loopTab()();
+// }
+
+// loopUrl().catch(echoError);
+// ------------------------------------------
+
+async function getDateFromGoogle() {
+  // setting
+  const driver = await build({
+    args: ["--headless", "--disable-gpu"],
     w3c: false,
-  };
+  });
 
-  await (await Manga.init(options)).loopUrl();
-})().catch(echoError);
+  // Go to Google URL
+  await driver.get("https://www.google.com/search?q=today+date");
+
+  const log = await getUrlContent(
+    // domain:RegExp
+    "google.com/search",
+
+    // Test target for regular expressions
+    "https://www.google.com/search?q=today+date",
+
+    // Get date from HTML Element
+    async (driver: WebDriver) => {
+      const day_card = await driver.findElement(By.className("card-section"));
+      const div = await day_card.findElement(By.css("div"));
+      const day_of_the_week = await (await day_card.findElement(By.css("div"))).getText();
+      return (await div.findElement(By.css("span")).getText()) + " " + day_of_the_week;
+    },
+
+    // Get locate from HTML Element
+    async (driver: WebDriver) => {
+      const elm = await driver.findElement(By.className("card-section"));
+      return await (await elm.findElement(By.className("vk_gy"))).getText();
+    },
+    driver,
+  );
+
+  console.log(log);
+  // write log
+  const today = moment().format("YYYY-MM-DD");
+  writeFiles(`src/selenium/logs/${today}.txt`, log ?? "Nothing data");
+
+  // pause command
+  execCommand("pause");
+}
+
+getDateFromGoogle().catch(echoError);
