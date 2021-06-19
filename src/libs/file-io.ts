0be04aee65621fb6302a81cb73338ce4199b9e6a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-06-20 03:50:17 +0900

    [Modify] Modify  file-io to async processing

--- src/libs/file-io.ts
+++ src/libs/file-io.ts
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, writeFile } from "fs";
+import { mkdir, writeFile } from "fs/promises";
 import chalk from "chalk";
 
 /**
@@ -6,22 +6,26 @@ import chalk from "chalk";
  * @param path - The file path to export to.
  * @param contents - What to write
  */
-export function writeFiles(path: string, contents: string | string[]): void {
-  // Make dir if not exist dir
-  const dir_path = path.replace(/logs.*\..*$/, "logs");
-  if (!existsSync(dir_path)) {
-    mkdirSync(dir_path);
+export async function writeFiles(path: string, contents: string | string[]): Promise<void> {
+  try {
+    // Make dir if not exist dir
+    const dir_path = path.replace(/([^/]+?)?$/, "");
+    mkdir(dir_path, { recursive: true });
+
+    // write file
+    await writeFile(path, stripAnsi(`${contents}`));
+    console.log(chalk`{green Completed writing the log.}`);
+
+    // error catch
+  } catch (error) {
+    console.log(chalk`{red Failed to write log.}\n${error}`);
   }
-  writeFile(path, stripAnsi(`${contents}`), (err) => {
-    // If the export fails
-    if (err) {
-      console.log(chalk`{red Failed to write log.}\n${err}`);
-      throw err;
-    }
-    console.log(chalk`{green Write log Complete.}`);
-  });
 }
 
+/**
+ * Remove color codes from strings.
+ * @param text string
+ */
 export function stripAnsi(text: string): string {
   return text.replace(
     /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
