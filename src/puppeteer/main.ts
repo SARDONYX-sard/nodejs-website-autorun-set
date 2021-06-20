e1435cb85f3a43eca06ca64aa59592ca4c6d38ab
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-06-20 15:14:56 +0900

    [Change] Change puppeteer to ts file

--- src/puppeteer/main.ts
+++ src/puppeteer/main.ts
@@ -1,5 +1,4 @@
-const puppeteer = require("puppeteer");
-// import * as puppeteer from "puppeteer";
+import { launch } from "puppeteer";
 
 // Unhandled promise rejection
 process.on("unhandledRejection", (error) => {
@@ -14,7 +13,8 @@ const articleUrlList = [
   "https://qiita.com/horikeso/items/bb255eede8a051dfa785",
 ];
 (async () => {
-  const browser = await puppeteer.launch({
+  // setting
+  const browser = await launch({
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
@@ -22,8 +22,10 @@ const articleUrlList = [
       "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
     ],
   });
+
+  const promiseList: Promise<any>[] = [];
   let index = 0;
-  promiseList = [];
+
   articleUrlList.forEach((targetUrl) => {
     promiseList.push(
       (async (index) => {
@@ -39,23 +41,27 @@ const articleUrlList = [
         console.log(index);
         const fileName = "src/puppeteer/img/" + index + ".png";
         await page.screenshot({ path: fileName, fullPage: true });
+        let title: string;
 
-        const result = await page.evaluate(() => [
-          document.querySelector('meta[property="og:title"]').getAttribute("content"),
-        ]);
+        const result = await page.evaluate(() => {
+          const $meta = document.querySelector('meta[property="og:title"]');
+          if ($meta instanceof Element) {
+            title = $meta.getAttribute("content") ?? "";
+          }
+          return [title];
+        });
 
         await page.close();
-
         return result;
       })(index),
     );
     index++;
   });
 
-  let articleTitleList = [];
+  let articleTitleList: string[] = [];
   await Promise.all(promiseList)
     .then((valueList) => {
-      valueList.forEach((value) => {
+      valueList.forEach((value: string) => {
         articleTitleList = articleTitleList.concat(value);
       });
     })
