7c7c3f80e01741cc7c9c0a3ab798b59da85230b7
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-06-24 16:37:15 +0900

    [Add] Add promise log

--- src/libs/error-handle.ts
+++ src/libs/error-handle.ts
@@ -1,7 +1,8 @@
 import chalk from "chalk";
 
 // handle promise rejection
-process.on("unhandledRejection", (error) => {
-  console.error(chalk`promise {red ERR!} ${error}`);
+process.on("unhandledRejection", (error, promise) => {
+  console.error(chalk`Error: {red ERR!} ${error}`);
+  console.error(chalk`promise: {red ERR!} ${promise}`);
   process.exit(1);
 });
