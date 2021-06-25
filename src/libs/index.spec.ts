049e0ac5726ce171d98203248aa70ffaf8d65177
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-06-26 02:14:21 +0900

    [Modify] Modify index.spec.ts

--- src/libs/index.spec.ts
+++ src/libs/index.spec.ts
@@ -1,7 +1,15 @@
-import { execCommand } from "./index";
+import { execCommand, sleep, writeFiles } from "./index";
 
-describe("execCommand", () => {
+describe("index", () => {
   it("should export `execCommand` function", () => {
     expect(execCommand).not.toBeUndefined();
+  });
+
+  it("should export `writeFiles` function", () => {
+    expect(writeFiles).not.toBeUndefined();
+  });
+
+  it("should export `sleep` function", () => {
+    expect(sleep).not.toBeUndefined();
   });
 });
--- src/selenium/libs/index.spec.ts
+++ src/selenium/libs/index.spec.ts
@@ -1,7 +1,15 @@
-import { build } from "./index";
+import { build, default_urls, switchNewTab } from "./index";
 
-describe("build", () => {
+describe("index", () => {
   it("should export `build` function", () => {
     expect(build).not.toBeUndefined();
+  });
+
+  it("should export `switchNewTab` function", () => {
+    expect(switchNewTab).not.toBeUndefined();
+  });
+
+  it("should export `default_urls` function", () => {
+    expect(default_urls).not.toBeUndefined();
   });
 });
